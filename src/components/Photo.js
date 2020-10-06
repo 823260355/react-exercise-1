b5df53a31b935743f420f52787d89f65ad26f0ec
LiYuan1229 <823260355@qq.com>
2020-10-06 17:49:53 +0800

    [liyuan]:finish avatar img

---
+++ src/components/Photo.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Avatar from '../assets/avatar.jpg';
+
+const Photo = () => (
+  <img
+    className="image-size image-location"
+    src={Avatar}
+    alt="avatar.jpg"
+  ></img>
+);
+
+export default Photo;
