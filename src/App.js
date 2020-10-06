db87707a8e5f3818b178d42b2374879bf972e880
LiYuan1229 <823260355@qq.com>
2020-10-06 18:08:46 +0800

    [liyuan]:finish hello

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import './App.scss';
 import Photo from './components/Photo';
+import Header from './components/Header';
 
 class App extends Component {
   render() {
     return (
       <main className="app">
         <Photo />
+        <Header />
       </main>
     );
   }
